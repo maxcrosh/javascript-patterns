caab9574affd7d575dc912635d3bdeea3ab48844
Maksim Makarov <maximmakarov1996@gmail.com>
2021-08-02 15:43:22 +0300

    Update array-operations.js

--- array-operations.js
+++ array-operations.js
@@ -1,1 +1,33 @@
+// Array functions
 
+/*
+forEach
+
+forEach function runs callback for each item in array.
+It returns undefined and doesn't modify array.
+Just runs instructions one per item.
+*/
+var fruits = ["Banana", "Apple"]
+
+fruits.forEach((item, index, array) => {
+  console.log(item, index, array)
+})
+
+// Output
+Banana 0 (2) ["Banana", "Apple"]
+Apple 1 (2) ["Banana", "Apple"]
+
+
+/*
+map
+
+map function runs callback for each item in array.
+It returns modified array according to instructions in callback.
+*/
+var numbers = [1,2,3]
+
+var sqrt_numbers = numbers.map(item => Math.pow(item, 2))
+console.log(sqrt_numbers)
+
+// Output
+// [1, 4, 9]
